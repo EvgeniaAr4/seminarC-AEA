f6454c5201bd0c924f5e7be786837a756a0ff0c2
Evgenia <olivia20011978@mail.ru>
2022-07-09 22:10:31 +0300

    +009;011

---
+++ Task009/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывести на экран четные числа от 1 до N
+
+int i=2;
+System.Console.WriteLine("введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+while(i<=N)
+{
+    System.Console.Write($"{i} ");
+    i=i+2;
+}
---
+++ Task011/Program.cs
@@ -0,0 +1,5 @@
+﻿// Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+System.Console.WriteLine("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b=a/10%10;
+System.Console.WriteLine(b);
