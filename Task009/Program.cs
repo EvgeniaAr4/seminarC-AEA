2109c474ab8544ecae180b409ba894469f933753
Evgenia <olivia20011978@mail.ru>
2022-07-09 22:26:46 +0300

    addition

--- Task009/Program.cs
+++ Task009/Program.cs
@@ -5,6 +5,9 @@ System.Console.WriteLine("введите число");
 int N = Convert.ToInt32(Console.ReadLine());
 while(i<=N)
 {
-    System.Console.Write($"{i} ");
-    i=i+2;
+    if (i%2==0)
+    {
+        System.Console.Write($"{i} ");
+    }
+    i=i+1;
 }
