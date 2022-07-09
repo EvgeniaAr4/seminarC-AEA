b197c23e7de516b2f5e59c66756c19e37b97c16f
Evgenia <olivia20011978@mail.ru>
2022-07-09 21:36:40 +0300

    init

---
+++ Task001/Program.cs
@@ -0,0 +1,7 @@
+﻿// С клавиатуры вводится целое число. Вывести квадрат числа 
+
+System.Console.WriteLine("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b;
+b=a*a;
+System.Console.WriteLine($"{a}*2={b}");
---
+++ Task002/Program.cs
@@ -0,0 +1,24 @@
+﻿// С клавиатуры вводятся два вещественных числа. 
+// Проверять является ли одно из них квадратом второго
+
+System.Console.WriteLine("введите первое число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите второе число b= ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b)
+{
+    System.Console.WriteLine("b является квадратом а");
+}
+else
+{
+    if (b * b == a)
+    {
+        System.Console.WriteLine("a является квадратом b");
+
+    }
+    else
+    {
+            System.Console.WriteLine("не является квадратом ");
+    }
+
+}
