70ac9d0161dcc1d7d0ea15f053e07e70d90eb50f
Evgenia <olivia20011978@mail.ru>
2022-07-10 17:35:37 +0300

    +007

---
+++ Task007/Program.cs
@@ -0,0 +1,9 @@
+﻿// Выяснить является ли число чётным.
+
+Console.WriteLine("input num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+System.Console.WriteLine("четное");
+else
+System.Console.WriteLine("нечетное");
+
