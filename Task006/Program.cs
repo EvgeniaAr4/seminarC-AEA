a680a85092accc9c20f8fd4c65bf78f4217af482
Evgenia <olivia20011978@mail.ru>
2022-07-10 14:16:57 +0300

    +006

---
+++ Task006/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу вычисления значения 
+//функции y = sin(a). (Класс Math)
+
+double x,y;
+System.Console.WriteLine("введите число");
+x = Convert.ToDouble(Console.ReadLine());
+y=Math.Sin(x);
+System.Console.WriteLine($"sin({x})={y}");
---
+++ Task008/Program.cs
@@ -0,0 +1,13 @@
+﻿// 
+
+int i=2;
+System.Console.WriteLine("введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+while(i<=N)
+{
+    if (i%2==0)
+    {
+        System.Console.Write($"{i} ");
+    }
+    i=i+1;
+}
