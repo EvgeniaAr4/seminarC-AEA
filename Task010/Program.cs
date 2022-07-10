7ffe02ba29ab2066673f7436929f6c74e8738506
Evgenia <olivia20011978@mail.ru>
2022-07-10 17:45:03 +0300

    +010

---
+++ Task010/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+
+System.Console.WriteLine("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>10)
+{
+a = a%10;
+System.Console.WriteLine($"{a}");
+}
+else
+{
+    System.Console.WriteLine("incorret");
+}
