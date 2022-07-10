1f352d5aff7d23c09aa2baa999189cbbd4d3074b
Evgenia <olivia20011978@mail.ru>
2022-07-10 18:53:58 +0300

    +012

---
+++ Task012/Program.cs
@@ -0,0 +1,25 @@
+﻿// Дано число из  диапозона  [10, 99]. Показать наибольшую цифру числа
+// Не поняла тз и сделала два варианта (в первом - наибольшее число из диапазона; во втором - наибольшие цифры в диапазоне)
+int a = 10;
+int b = 99;
+int max = a;
+if ( a < b )
+{
+     max = b;
+System.Console.WriteLine(max);
+}
+else
+{
+System.Console.WriteLine(a);
+}
+
+/*
+int a = 10;
+            int b = 99;
+            for (; b !=0; b/=10)
+            {
+                int r = b % 10;
+                if (r > a) a = r;
+                System.Console.WriteLine(r);
+            }
+/*
