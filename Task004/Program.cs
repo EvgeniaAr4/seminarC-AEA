9739d4e401901143f71cca1eaf2613ecc5f0d66a
Evgenia <olivia20011978@mail.ru>
2022-07-10 13:54:33 +0300

    +004

---
+++ Task004/Program.cs
@@ -0,0 +1,36 @@
+﻿// По заданному с клавиатуры номеру дня недели вывести его название
+
+Console.WriteLine("input number of a day: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day < 1 || day > 7)
+{
+    Console.Write("incorrect day");
+}
+
+ else
+switch (day)
+{
+    case 1: 
+         System.Console.WriteLine("monday");
+         break;
+    case 2: 
+         System.Console.WriteLine("tuesday");
+         break; 
+    case 3: 
+         System.Console.WriteLine("wednesday");
+         break;  
+    case 4: 
+         System.Console.WriteLine("thursday");
+         break;   
+    case 5: 
+         System.Console.WriteLine("firday");
+         break;  
+    case 6: 
+         System.Console.WriteLine("saturday");
+         break; 
+    case 7: 
+         System.Console.WriteLine("sunday");
+         break;                 
+
+}   
