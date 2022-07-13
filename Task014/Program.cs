e50a40e5c1e95b201d189a3f43e94488b61ddd90
Evgenia <olivia20011978@mail.ru>
2022-07-13 11:47:47 +0300

    +016

---
+++ Task014/Program.cs
@@ -0,0 +1,17 @@
+﻿// С клавиатуры вводятся два числа a и b. Выяснить, кратно ли число a числу b, если нет, вывести остаток от деления a на b.
+
+System.Console.WriteLine("введите первое число a = ");
+double a = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("введите второе число b= ");
+double b = Convert.ToDouble(Console.ReadLine());
+if (a%b == 0){
+    System.Console.WriteLine("кратно");
+}
+else {
+    if (a%b != 0)
+    {
+        double sum = a%b;
+        sum = sum*10;
+        System.Console.WriteLine(sum);
+    }
+}
---
+++ Task016/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+bool b,c,d;
+b=true;
+b=false;
+System.Console.WriteLine("введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+b=x%7==0;
+c=x%23==0;
+d=b && c;
+System.Console.WriteLine(x);
+System.Console.WriteLine(d);
