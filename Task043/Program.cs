542275182f83caf8079a88d9418d617160d7520b
Evgenia <olivia20011978@mail.ru>
2022-08-06 14:53:35 +0300

    +task

---
+++ Task043/Program.cs
@@ -0,0 +1,15 @@
+﻿// С клавиатуры вводится число N. Затем вводятся N чисел. Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.Write("Введите числа : ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов больше нуля: {count}");
---
+++ Task046/Program.cs
@@ -0,0 +1,20 @@
+﻿// С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+
+Console.WriteLine("До какого числа считать ряд Фибоначчи?");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int first = 1;
+Console.Write("{0} ", first);
+int second = 1;
+Console.Write("{0} ", second);
+int sum = 0;
+
+while (number >= sum)
+            {
+                sum = first + second;
+                
+                    Console.Write("{0} ", sum);
+                    
+                        first = second;
+                        second = sum;                
+                }
